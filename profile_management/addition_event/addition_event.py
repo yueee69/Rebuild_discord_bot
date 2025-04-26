67230587cf3a73c61c3d4c7dfa5334febe8fadfb
yueee69 <xiulih87@gmail.com>
2025-04-26 17:23:06 +0800

    fix: - 刪除多餘的資料夾(event初期設計廢案)

--- profile_management/addition_event/addition_event.py
+++
@@ -1,5 +0,0 @@
-class Event:
-    @staticmethod
-    def driver(context: object):
-        pass
-        #檢查狀態 > 執行抽取 > 執行抽到的事件 > return
--- profile_management/addition_event/events.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Basic_event(ABC):
-    @abstractmethod
-    def event():
-        pass
